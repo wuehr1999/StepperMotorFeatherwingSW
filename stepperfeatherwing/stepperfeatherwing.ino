033ebae7006206605877c576afc12fe2f03075f6
Bernhard Mayer <bernhard@generationmake.de>
2020-06-24 01:34:44 +0200

    added defines for some registers

--- stepperfeatherwing/stepperfeatherwing.ino
+++ stepperfeatherwing/stepperfeatherwing.ino
@@ -57,20 +57,20 @@ void loop() {
 //    tmc1.move(0, false); // should be one turn back with 1.8 degrees and 256 micro steps
 //    tmc2.move(0, false); // should be one turn back with 1.8 degrees and 256 micro steps
 //    digitalWrite(LED_BUILTIN, LOW);
-    tmc1.speed(30000); // should be one turn with 1.8 degrees and 256 micro steps
-    tmc2.speed(30000); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc1.speed(30000,0); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc2.speed(30000,0); // should be one turn with 1.8 degrees and 256 micro steps
     digitalWrite(LED_BUILTIN, HIGH);
     delay(2000);
-    tmc1.speed(100000); // should be one turn with 1.8 degrees and 256 micro steps
-    tmc2.speed(100000); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc1.speed(100000,0); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc2.speed(100000,0); // should be one turn with 1.8 degrees and 256 micro steps
     digitalWrite(LED_BUILTIN, LOW);
     delay(2000);
-    tmc1.speed(10000); // should be one turn with 1.8 degrees and 256 micro steps
-    tmc2.speed(10000); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc1.speed(10000,1); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc2.speed(10000,1); // should be one turn with 1.8 degrees and 256 micro steps
     digitalWrite(LED_BUILTIN, HIGH);
     delay(2000);
-    tmc1.speed(00000); // should be one turn with 1.8 degrees and 256 micro steps
-    tmc2.speed(00000); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc1.speed(00000,1); // should be one turn with 1.8 degrees and 256 micro steps
+    tmc2.speed(00000,1); // should be one turn with 1.8 degrees and 256 micro steps
     digitalWrite(LED_BUILTIN, LOW);
   }
   delay(4000);
